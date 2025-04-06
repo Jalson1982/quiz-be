51258f2032b3c48d9e33dbf7d1d3c22b38bdd86c
Amer <amerlopo5@gmail.com>
2025-04-06 12:51:39 +0200

    User roles

--- src/config/index.js
+++ src/config/index.js
@@ -19,7 +19,7 @@ export default {
     algorithm: "HS256",
   },
   cors: {
-    origin: process.env.CORS_ORIGIN || "*",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
--- src/models/Question.js
+++ src/models/Question.js
@@ -19,6 +19,20 @@ const questionSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  createdBy: {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
 });
 
 export default mongoose.model("Question", questionSchema);

47ed180dbb27a31cffaa058238411709f76e44b0
Jalson1982 <jasmin.fajkic@gmail.com>
2025-04-08 23:01:08 +0200

    Try swagger fix

--- src/swagger.js
+++ src/swagger.js
@@ -24,13 +24,5 @@ const options = {
 const swaggerSpec = swaggerJsDoc(options);
 
 export default (app) => {
-  app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, {
-      customCss:
-        ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
-      customCssUrl: CSS_URL,
-    })
-  );
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };

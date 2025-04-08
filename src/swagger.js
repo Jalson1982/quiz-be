d5e0dba3946d8c99f3f95cebf120c9cb4f7d2e39
Jalson1982 <jasmin.fajkic@gmail.com>
2025-04-08 22:53:10 +0200

    Try swagger fix

--- src/swagger.js
+++ src/swagger.js
@@ -1,6 +1,9 @@
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const options = {
   definition: {
     openapi: "3.0.0",
@@ -21,5 +24,13 @@ const options = {
 const swaggerSpec = swaggerJsDoc(options);
 
 export default (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, {
+      customCss:
+        ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+      customCssUrl: CSS_URL,
+    })
+  );
 };
